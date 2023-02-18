d0c0fd321278ef6bab9c0a86933f5fe2da714e60
Hamilton Oh <hamiltonoh@gmail.com>
2023-02-18 12:36:30 -0800

    build: added jupyter and ipykernel

--- src/OrganAge/pycounts.py
+++ src/OrganAge/pycounts.py
@@ -3,14 +3,45 @@ from string import punctuation
 
 
 def load_text(input_file):
-    """Load text from a text file and return as a string."""
+    """Load text from a text file and return as a string.
+
+    Parameters
+    ----------
+    input_file : str
+        Path to text file.
+
+    Returns
+    -------
+    str
+        Text file contents.
+
+    Examples
+    --------
+    >>> load_text("text.txt")
+    """
     with open(input_file, "r") as file:
         text = file.read()
     return text
 
 
 def clean_text(text):
-    """Lowercase and remove punctuation from a string."""
+    """Lowercase and remove punctuation from a string.
+
+    Parameters
+    ----------
+    text : str
+        Text to clean.
+
+    Returns
+    -------
+    str
+        Cleaned text.
+
+    Examples
+    --------
+    >>> clean_text("Early optimization is the root of all evil!")
+    'early optimization is the root of all evil'
+    """
     text = text.lower()
     for p in punctuation:
         text = text.replace(p, "")
@@ -18,7 +49,25 @@ def clean_text(text):
 
 
 def count_words(input_file):
-    """Count unique words in a string."""
+    """Count words in a text file.
+
+    Words are made lowercase and punctuation is removed
+    before counting.
+
+    Parameters
+    ----------
+    input_file : str
+        Path to text file.
+
+    Returns
+    -------
+    collections.Counter
+        dict-like object where keys are words and values are counts.
+
+    Examples
+    --------
+    >>> count_words("text.txt")
+    """
     text = load_text(input_file)
     text = clean_text(text)
     words = text.split()
