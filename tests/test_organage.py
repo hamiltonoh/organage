35310077d15c8852d143296a800c6cca37c273c1
Hamilton Oh <hamiltonoh@gmail.com>
2023-02-19 12:05:29 -0800

    build: estimate_organ_ages complete

--- tests/test_organage.py
+++ tests/test_organage.py
@@ -1,15 +1,15 @@
 from organage.OrganAge import CreateOrganAgeObject
 import pandas as pd
-import numpy as np
 
-def test_add_data():
+def test_OrganAge():
     data = CreateOrganAgeObject()
-
-    md_hot = pd.read_csv("tests/md_hot.csv").set_index("Barcode")
-    df_prot = pd.read_csv("tests/df_prot.csv").set_index("Barcode")
+    md_hot = pd.read_csv("tests/md_hot.csv").set_index("ID")
+    df_prot = pd.read_csv("tests/df_prot.csv").set_index("ID")
 
     # sample metadata data with Age and Sex_F
     data.add_data(md_hot, df_prot)
-    assert len(data.md_hot) == 5, "user data loaded incorrectly"
+    data.normalize(assay_version="v4.1")
+
+    dfres = data.estimate_organ_ages()
 
 
