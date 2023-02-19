3011947f97dd2cdeec64af9b0b48eec5530fb973
Hamilton Oh <hamiltonoh@gmail.com>
2023-02-18 17:41:29 -0800

    fix: fn fix

--- src/organage/OrganAge.py
+++ src/organage/OrganAge.py
@@ -10,44 +10,43 @@ class CreateOrganAgeObject:
 
     # init method or constructor
     def __init__(self,
-                 path_scale_dict='v4_to_v4.1_scale_dict.json',
-                 path_seed_dict='Bootstrap_and_permutation_500_seed_dict.json',
+                 path_version_scale_factors='v4_to_v4.1_scale_dict.json',
                  path_organ_plist_dict='tissue_pproteinlist_5k_dict_gtex_tissue_enriched_fc4_stable_proteins_seqid.json',
+                 path_bootstrap_seeds='Bootstrap_and_permutation_500_seed_dict.json',
                  path_models_dir='data/ml_models/Covance/Zprot_stableps_perf95/'
                  ):
 
-        self.data_and_model_paths = {"path_scale_dict": path_scale_dict,
-                                     "path_seed_dict": path_seed_dict,
+        self.data_and_model_paths = {"path_version_scale_factors": path_version_scale_factors,
                                      "path_organ_plist_dict": path_organ_plist_dict,
-                                     "path_models_dir": path_models_dir}
+                                     "path_bootstrap_seeds": path_bootstrap_seeds,
+                                     "path_models_dir": path_models_dir
+                                     }
         self.load_data_and_models()
+        del self.data_and_model_paths
 
     def load_data_and_models(self):
 
         # Seqid:scale_factor dictionary
-        scale_dict = json.load(resources.open_text("organage.data", self.data_and_model_paths["path_scale_dict"]))
-        self.scale_dict = scale_dict
-
-        # 500 bootstrap seed list
-        all_seeds = json.load(resources.open_text("organage.data", self.data_and_model_paths["path_seed_dict"]))["BS_Seed"]
-        self.all_seeds = all_seeds
+        version_scale_factors = json.load(resources.open_text("organage.data", self.data_and_model_paths["path_version_scale_factors"]))
+        self.version_scale_factors = version_scale_factors
 
         # organ:proteinlist dictionary
         organ_plist_dict = json.load(resources.open_text("organage.data", self.data_and_model_paths["path_organ_plist_dict"]))
         self.organ_plist_dict = organ_plist_dict
 
-        # models
+        # 500 bootstrapped models
+        bootstrap_seeds = json.load(resources.open_text("organage.data", self.data_and_model_paths["path_bootstrap_seeds"]))["BS_Seed"]
         models_dict = {}
         for organ in organ_plist_dict:
             models_dict[organ] = {}
             models_dict[organ]["aging_models"] = []
 
-            # load protein scaler
-            fn_protein_scaler = 'Covance_all_based_protein_zscore_scaler.pkl'
+            # load protein zscore scaler
+            fn_protein_scaler = 'Covance_all_based_'+organ+'_protein_zscore_scaler.pkl'
             loaded_model = pickle.loads(resources.read_binary('organage.data.ml_models.Covance.Zprot_stableps_perf95.' + organ, fn_protein_scaler))
             models_dict[organ]["prot_scaler"] = loaded_model
 
-            # age gap scaler
+            # age gap zscore scaler
             fn_agegap_scaler = 'Covance_all_Zprot_stableps_perf95_lasso_'+organ+'_agegap_zscore_scaler.pkl'
             loaded_model = pickle.loads(resources.read_binary('organage.data.ml_models.Covance.Zprot_stableps_perf95.' + organ, fn_agegap_scaler))
             models_dict[organ]["agegap_scaler"] = loaded_model
@@ -58,7 +57,7 @@ class CreateOrganAgeObject:
             models_dict[organ]["age_prediction_lowess"] = loaded_model
 
             # load all aging models
-            for seed in all_seeds:
+            for seed in bootstrap_seeds:
                 fn_aging_model = 'Covance_all_Zprot_stableps_perf95_lasso_'+organ+'_seed'+str(seed)+'_aging_model.pkl'
                 loaded_model = pickle.loads(resources.read_binary('organage.data.ml_models.Covance.Zprot_stableps_perf95.'+ organ, fn_aging_model))
                 models_dict[organ]["aging_models"].append(loaded_model)
